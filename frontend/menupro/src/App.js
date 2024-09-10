04b9ae0fd1fe1bf93b76dc6a20d531e5b285aa12
Felinotordo <felinotordo99@gmail.com>
2024-09-09 23:18:00 -0300

    first code of frontend

---
+++ frontend/menupro/src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <div className="m-0 p-0 min-h-screen flex flex-col justify-between">
+        <div className="bg-[#4CAF50] w-full h-[50px] flex items-center">
+          <h1 className="text-[20px] font-bold text-white m-[15px] bg-transparent">
+            MenuPro
+          </h1>
+        </div>
+
+        <footer className="bg-[#4CAF50] w-full h-[50px] flex items-center justify-center">
+          <p className="text-white">© 2024 MenuPro</p>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
