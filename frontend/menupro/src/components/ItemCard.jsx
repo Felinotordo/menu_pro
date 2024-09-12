40922f379e413d28fd295f3d45ae1914ca1b236d
Felinotordo <felinotordo99@gmail.com>
2024-09-12 07:20:34 -0300

    add currency

--- frontend/menupro/src/components/ItemCard.jsx
+++ frontend/menupro/src/components/ItemCard.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 
 const ItemCard = (props) => {
+
+  const formatCurrency = (amount, currency = 'ARS') => {
+    return new Intl.NumberFormat('es-AR', {
+      style: 'currency',
+      currency: currency,
+    }).format(amount);
+  };
+
   return (
     <>
-    <div className="w-full h-auto flex justify-center items-start m-0">
-      <div className="w-[360px] h-[120px] mt-[25px] bg-[#D9D9D9] rounded-[5px] flex justify-start items-center inset-y-0 left-0">
-        <div className="flex flex-col">
-          <p>{props.name}</p>
-          <p>{props.description}</p>
-          <p>{props.price}</p>
+      <div className="w-full h-auto flex justify-center items-start m-0">
+        <div className="w-[360px] h-[120px] mt-[25px] bg-[#D9D9D9] rounded-[5px] flex justify-start items-center inset-y-0 left-0">
+          <div className="flex flex-col mx-[10px] w-full">
+            <div className="flex flex-col">
+              <p className="font-bold text-[15px]">{props.name}</p>
+              <p>{props.description}</p>
+            </div>
+            <div className="flex justify-end">
+              <p className="font-bold text-[15px]">{formatCurrency(props.price)}</p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </>
+    </>
   );
 };
 
